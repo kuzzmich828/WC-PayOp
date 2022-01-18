b1346798fb38fa2398fd258b9d00d92b2e30dec5
kuzzmich <kuzzmich828@gmail.com>
2022-01-18 10:55:19 +0200

    Modify IPN

--- assets/js/payop.js
+++ assets/js/payop.js
@@ -5,13 +5,12 @@ jQuery( document ).ready(function (){
     var settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://app.stage.payop.com/v1/instrument-settings/payment-methods/available-for-application/a3cb469c-27f3-4c4c-9b96-27bc463f3e69",
+        "url": "https://app.stage.payop.com/v1/instrument-settings/payment-methods/available-for-application/XXXXXXX",
         "method": "GET",
         "headers": {
             "content-type": "application/json",
             "authorization": "",
-            "cache-control": "no-cache",
-            "postman-token": "db6eed92-0afe-4095-74b0-beb788dd97e4"
+            "cache-control": "no-cache"
         }
     }
 
